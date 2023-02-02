d6f23f6fe64b16ec934170b76cb7568f2cf6d0b0
Jacob Parris <typefortyproductions@gmail.com>
2023-02-02 15:41:13 -0500

    Adding/attempting Discord Bot Integration

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
+const Discord = require("discord.js");
+const config = require("./discord_integration/config.json");
 const mtg = require('mtgsdk');
 const inquirer = require("inquirer");
+const { GatewayIntentBits } = require("discord.js");
 
 //randcard = Math.floor(Math.random() * 5000)
+
+const client = new Discord.Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+    ]
+});
+
+const prefix = "!"
+
+client.on("messageCreate", function(message) {
+    
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+  
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+  
+    if (command === "ping") {
+      const timeTaken = Date.now() - message.createdTimestamp;
+      message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+    }
+  
+    else if (command === "sum") {
+      const numArgs = args.map(x => parseFloat(x));
+      const sum = numArgs.reduce((counter, x) => counter += x);
+      message.reply(`The sum of all the arguments you provided is ${sum}!`);
+    }
+  });
+
+client.login(config.BOT_TOKEN);
 
 const question = [
 
@@ -53,4 +88,8 @@ function init() {
         .catch((err) => console.error(err));
 }
 
-init();
+
+
+
+
+//init();
