898049a35c0df9a428ae7b4b5134fbfcec71c221
Jacob Parris <typefortyproductions@gmail.com>
2023-02-09 12:08:39 -0500

    Implemented Embed to Scry and update presentation design

--- commands/scrycard.js
+++ commands/scrycard.js
@@ -47,16 +47,20 @@ module.exports = {
         const embed = new EmbedBuilder()
 			.setColor(0xEFFF00)
 			.setTitle(mtg_scry[0].name)
+            .setThumbnail(`https://www.vippng.com/png/detail/419-4193845_magic-the-gathering-clipart-planeswalker-magic-the-gathering.png`)
             .setDescription(`${mtg_scry[0].supertypes} ${mtg_scry[0].types} - ${mtg_scry[0].subtypes}`)
 			.setURL(`${mtg_scry[0].imageUrl}`|| `https://www.discord.js`)
 			.addFields(
-                { name: 'Description', value: `${mtg_scry[0].text}`},
+                
 				{ name: 'Total Mana Cost', value: `${mtg_scry[0].cmc}`, inline: true},
 				{
 					name: 'Mana Color',
 					value: `${color_emoji}`,
                     inline: true,
 				},
+                { name: `Rarity`, value: `${mtg_scry[0].rarity}`, inline: true},
+                { name: 'Description', value: `${mtg_scry[0].text}`},
+                { name: `Flavor`, value: `${mtg_scry[0].flavor}`},
             );
 
         return interaction.editReply({embeds: [embed]}).catch(err => console.log(err))
